0f228638db594d89f9cb191afd47bd13b72ad177
Benjamin Nguyen <solidiquis@Benjamins-MacBook-Pro.local>
2019-04-14 15:14:20 -0700

    Debugging

--- Scribnotes/settings.py
+++ Scribnotes/settings.py
@@ -1,6 +1,7 @@
 import dj_database_url
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -119,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
     ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 CKEDITOR_CONFIGS = {
