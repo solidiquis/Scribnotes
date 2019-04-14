b2c5595b1107db8e0ba176cff5af8e9f30065630
Benjamin Nguyen <solidiquis@Benjamins-MacBook-Pro.local>
2019-04-14 16:23:52 -0700

    Debugging

--- Scribnotes/settings.py
+++ Scribnotes/settings.py
@@ -11,7 +11,7 @@ STATIC_DIR = os.path.join(STATIC_ROOT, 'notes')
 SECRET_KEY = os.environ['SCRIBNOTES_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['scribnotesapp.herokuapp.com', 'localhost',]
 
