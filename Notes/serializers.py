73a7f448de0a23a0514fe2f902ddeddba7739bcd
Benjamin Nguyen <solidiquis@Benjamins-MacBook-Pro.local>
2019-04-15 12:59:53 -0700

    Added user field to DRF form

--- Notes/serializers.py
+++ Notes/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Term, Course, ClassNote
 
@@ -7,26 +8,29 @@ class TermSerializer(serializers.HyperlinkedModelSerializer):
     JSON.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        user = self.context['request'].user
+
+        if user.is_authenticated:
+            self.fields['user'].queryset = get_user_model().objects.filter(
+                username=user.username,
+            )
+        else:
+            self.fields['user'].queryset = get_user_model().objects.none()
+
     class Meta():
         """
         Fields of Term instances that will get serialized/deserialized.
         """
         model = Term
-        fields = ('school', 'year', 'session', 'term_slug',)
+        fields = ('user', 'school', 'year', 'session', 'term_slug',)
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializes and deserializes Course instances into representations such as
     JSON.
     """
-
-    class Meta():
-        """
-        Fields of Term instances that will get serialized/deserialized.
-        """
-        model = Course
-        fields = ('title', 'course_code', 'course_slug', 'term',)
-
     def __init__(self, *args, **kwargs):
         """
         Dynamically filters term choices by limiting options to the terms
@@ -34,10 +38,21 @@ class CourseSerializer(serializers.HyperlinkedModelSerializer):
         """
         super().__init__(*args, **kwargs)
         user = self.context['request'].user
+
         if user.is_authenticated:
+            self.fields['user'].queryset = get_user_model().objects.filter(
+                username=user.username,
+            )
             self.fields['term'].queryset = Term.objects.filter(user=user)
         else:
             self.fields['term'].queryset = Term.objects.none()
+
+    class Meta():
+        """
+        Fields of Term instances that will get serialized/deserialized.
+        """
+        model = Course
+        fields = ('user', 'title', 'course_code', 'course_slug', 'term',)
 
 class ClassNoteSerializer(serializers.HyperlinkedModelSerializer):
     """
@@ -50,7 +65,7 @@ class ClassNoteSerializer(serializers.HyperlinkedModelSerializer):
         Fields of ClassNote instances that will get serialized/deserialized.
         """
         model = ClassNote
-        fields = ('title', 'body', 'note_slug', 'course',)
+        fields = ('user', 'title', 'body', 'note_slug', 'course',)
 
     def __init__(self, *args, **kwargs):
         """
@@ -59,7 +74,11 @@ class ClassNoteSerializer(serializers.HyperlinkedModelSerializer):
         """
         super().__init__(*args, **kwargs)
         user = self.context['request'].user
+
         if user.is_authenticated:
+            self.fields['user'].queryset = get_user_model().objects.filter(
+                username=user.username,
+            )
             self.fields['course'].queryset = Course.objects.filter(user=user)
         else:
             self.fields['course'].queryset = Course.objects.none()
